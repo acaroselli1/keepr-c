c04113e0b945e8ce2915596542b6538a14c58653
Alex Caroselli <alexandersylvia9@msn.com>
2017-09-29 22:06:03 -0600

    form work

--- wwwsrc/src/components/Hello.vue
+++ wwwsrc/src/components/Hello.vue
@@ -2,22 +2,23 @@
   <div class="hello">
     <h1>Welcome to <span>k</span>eepr</h1>
     <ul>
-      <li>Login</li>
-      <li>Register</li>
+      <li @click="showLogin">Login</li>
+      <li @click="showRegister">Register</li>
+      <li @click="browse">Browse</li>
    
-    <form v-show ="registerSeen"> 
-     <input>
-     <input>  
-     <input> 
-     <input>
-     <button btn btn-default>Register</button>
+    <form v-show ="registerShown"> 
+     <input type ="text" v-model="user.name" placeholder="Username">
+     <input type = "text" v-model="user.email" placeholder="Email">  
+     <input type = "password" v-model="user.password" placeholder ="Password"> 
+     <input type = "password" v-model="user.passwordConfirm" placeholder ="Confirm Password">
+     <button class="btn btn-primary">Register</button>
     </form>
   
     
-    <form v-show="loginSeen">
-    <input>
-    <input>
-    <button btn btn-default>Login</button>
+    <form v-show="loginShown">
+    <input type = "text" v-model="user.email" placeholder="Email">  
+    <input type = "password" v-model="user.password" placeholder ="Password"> 
+    <button class="btn btn-primary">Login</button>
     </form>
     
    
@@ -31,11 +32,34 @@ export default {
   name: 'hello',
   data () {
     return {
-      registerSeen:false,
-      loginSeen:false
+     
+     user:{
+       name:'',
+      email:'',
+   password:'',
+   passwordConfirm:''
+    },
+     
+     
+     
+     
+      registerShown:false,
+      loginShown:false,
+
 
     }
-  }
+  },
+      methods:{
+        showLogin(){
+          this.loginShown = true
+          this.registerShown = false
+        },
+        showRegister(){
+          this.registerShown = true
+          this.loginShown = false
+
+        }
+      }
 }
 </script>
 
@@ -52,7 +76,9 @@ ul {
 
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin-top: 3vh;
+  margin-bottom:3vh;
+  margin-left:1vw;
 }
 
 li {
@@ -66,6 +92,7 @@ li:hover{
 
 span{
  color:skyblue;
+ text-shadow:1px 1px 8px blue;
 }
 
 
